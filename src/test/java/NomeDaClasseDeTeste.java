3e5026ad7986fb437982226ec9e546d23d34751d
edsonsalvador30 <edsonsalvador30>
2018-01-10 14:30:25 +0000

    teste 3

---
+++ src/test/java/NomeDaClasseDeTeste.java
@@ -0,0 +1,39 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+/**
+ * Classe de Teste que representa ...
+ * 
+ * @author edson.fonseca@techne.com.br
+ * @version 1.0
+ * @since 2018-01-10
+ *
+ */
+ 
+public class NomeDaClasseDeTeste {
+
+  /**
+   * Preparação antes dos testes
+   */
+  @Before
+  public void setUp() {
+     // TODO
+  }
+
+
+  @Test
+  public void sumTest() {
+    assertEquals(2, 1+1);
+  }
+
+  /**
+   * Limpeza após os testes
+   */
+  @After
+  public void tearDown() {
+     // TODO
+  }
+
+}
